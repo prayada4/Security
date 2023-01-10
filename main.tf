293ff0bdc2245bf7854af3939bfeca3af17ab653
prayada4 <121497330+prayada4@users.noreply.github.com>
2023-01-10 20:33:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
+data "terraform_remote_state" "vpc" {
+  backend= "remote"
+  config = {
+    organization = "demo-tfe"
+    workspaces = {
+      name = "Network-Team"
+    }
+  }
+}
+
+
 module "ec2-security-group" {
   source  = "app.terraform.io/demo-tfe/ec2-security-group/aws"
-  version = "1.0.2"
+  version = "1.0.3"
+  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
 }
